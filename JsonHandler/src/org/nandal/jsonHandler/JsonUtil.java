cf165101cda4d6960da22146ad4c1639a4baa156
nandal <sandeep@nandal.in>
2018-05-09 09:50:26 +0530

    Sample JMeter Plugin Added

---
+++ JsonHandler/src/org/nandal/jsonHandler/JsonUtil.java
@@ -0,0 +1,12 @@
+package org.nandal.jsonHandler;
+
+import org.apache.jmeter.util.JMeterUtils;
+import org.apache.oro.text.regex.Pattern;
+import org.apache.oro.text.regex.Perl5Matcher;
+
+public class JsonUtil {
+	public static boolean validateSchema(String toCheck, Pattern pattern) {
+		Perl5Matcher localMatcher = JMeterUtils.getMatcher();
+		return localMatcher.contains(toCheck, pattern);
+	}
+}
